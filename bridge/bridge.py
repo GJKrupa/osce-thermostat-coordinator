7c5bab82adb1a800871ac6388284ad29c77f9fb9
Gerard Krupa <github@bogbrushzone.co.uk>
2019-01-10 12:17:06 +0000

    Split LoRa/MQTT bridge from the business logic app

---
+++ bridge/bridge.py
@@ -0,0 +1,110 @@
+import paho.mqtt.client as mqtt
+from time import sleep
+from SX127x.LoRa import *
+from SX127x.LoRaArgumentParser import LoRaArgumentParser
+from SX127x.board_config import BOARD
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("$SYS/#")
+    client.subscribe("LORA/send")
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("mqtt", 1883, 60)
+
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+client.loop_start()
+
+BOARD.setup()
+
+parser = LoRaArgumentParser("Continous LoRa receiver.")
+class LoRaRcvCont(LoRa):
+    def __init__(self, verbose=False):
+        super(LoRaRcvCont, self).__init__(verbose)
+        self.set_mode(MODE.SLEEP)
+        self.set_dio_mapping([0] * 6)
+
+    def on_rx_done(self):
+        BOARD.led_on()
+        print("\nRxDone")
+        self.clear_irq_flags(RxDone=1)
+        payload = self.read_payload(nocheck=True)
+        payload_txt = "".join(map(chr, payload))
+        print(payload_txt)
+        client.publish("LORA/recv", payload_txt)
+        self.set_mode(MODE.SLEEP)
+        self.reset_ptr_rx()
+        BOARD.led_off()
+        self.set_mode(MODE.RXCONT)
+
+    def on_tx_done(self):
+        print("\nTxDone")
+        print(self.get_irq_flags())
+
+    def on_cad_done(self):
+        print("\non_CadDone")
+        print(self.get_irq_flags())
+
+    def on_rx_timeout(self):
+        print("\non_RxTimeout")
+        print(self.get_irq_flags())
+
+    def on_valid_header(self):
+        print("\non_ValidHeader")
+        print(self.get_irq_flags())
+
+    def on_payload_crc_error(self):
+        print("\non_PayloadCrcError")
+        print(self.get_irq_flags())
+
+    def on_fhss_change_channel(self):
+        print("\non_FhssChangeChannel")
+        print(self.get_irq_flags())
+
+    def start(self):
+        self.reset_ptr_rx()
+        self.set_mode(MODE.RXCONT)
+        while True:
+            sleep(.5)
+            rssi_value = self.get_rssi_value()
+            status = self.get_modem_status()
+            sys.stdout.flush()
+            sys.stdout.write("\r%d %d %d" % (rssi_value, status['rx_ongoing'], status['modem_clear']))
+
+
+lora = LoRaRcvCont(verbose=False)
+args = parser.parse_args(lora)
+
+lora.set_mode(MODE.STDBY)
+lora.set_freq(868.1)
+lora.set_pa_config(pa_select=1)
+
+print(lora)
+assert(lora.get_agc_auto_on() == 1)
+
+try:
+    lora.start()
+except KeyboardInterrupt:
+    sys.stdout.flush()
+    print("")
+    sys.stderr.write("KeyboardInterrupt\n")
+finally:
+    sys.stdout.flush()
+    print("")
+    lora.set_mode(MODE.SLEEP)
+    print(lora)
+    BOARD.teardown()
