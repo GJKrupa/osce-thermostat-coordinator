83683cbe88d94977416009087edf10350b6858c9
Gerard Krupa <github@bogbrushzone.co.uk>
2019-01-10 12:22:40 +0000

    Removed unnecessary SYS subscription

--- app/app.py
+++ app/app.py
@@ -1,16 +1,11 @@
 import paho.mqtt.client as mqtt
 from time import sleep
 
-# The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
 
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    client.subscribe("$SYS/#")
     client.subscribe("LORA/recv")
 
-# The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
 
@@ -20,8 +15,4 @@ client.on_message = on_message
 
 client.connect("mqtt", 1883, 60)
 
-# Blocking call that processes network traffic, dispatches callbacks and
-# handles reconnecting.
-# Other loop*() functions are available that give a threaded interface and a
-# manual interface.
 client.loop_forever()
--- bridge/bridge.py
+++ bridge/bridge.py
@@ -4,16 +4,10 @@ from SX127x.LoRa import *
 from SX127x.LoRaArgumentParser import LoRaArgumentParser
 from SX127x.board_config import BOARD
 
-# The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
-
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    client.subscribe("$SYS/#")
     client.subscribe("LORA/send")
 
-# The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
 
@@ -23,10 +17,6 @@ client.on_message = on_message
 
 client.connect("mqtt", 1883, 60)
 
-# Blocking call that processes network traffic, dispatches callbacks and
-# handles reconnecting.
-# Other loop*() functions are available that give a threaded interface and a
-# manual interface.
 client.loop_start()
 
 BOARD.setup()
