0937b8cfcf47af4eada17cc9cfbe0f3c3f7169f3
Gerard Krupa <github@bogbrushzone.co.uk>
2019-01-10 13:14:23 +0000

    Add handling of SETTEMP command

--- app/app.py
+++ app/app.py
@@ -9,7 +9,7 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
     msg_txt = str(msg.payload)
-    if (.startswith("SETTEMP,")):
+    if (msg_text.startswith("SETTEMP,")):
         cmd, value = msg_txt.split(",", 1)
         temp = int(value)
         if (value >= 10 and value <= 30):
