c708bab84da25135ea9e2eafa94ddb3e3957d25f
Gerard Krupa <github@bogbrushzone.co.uk>
2019-01-10 13:23:15 +0000

    Add handling of SETTEMP command

--- app/app.py
+++ app/app.py
@@ -7,18 +7,21 @@ def on_connect(client, userdata, flags, rc):
     client.subscribe("LORA/recv")
 
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
-    msg_txt = str(msg.payload)
-    if (msg_text.startswith("SETTEMP,")):
-        println("Set temperature message")
-        cmd, value = msg_txt.split(",", 1)
-        temp = int(value)
-        if (value >= 10 and value <= 30):
-            client.publish("LORA/send", "APPLYTEMP," + value)
+    try:
+        print(msg.topic+" "+str(msg.payload))
+        msg_txt = str(msg.payload)
+        if (msg_text.startswith("SETTEMP,")):
+            println("Set temperature message")
+            cmd, value = msg_txt.split(",", 1)
+            temp = int(value)
+            if (value >= 10 and value <= 30):
+                client.publish("LORA/send", "APPLYTEMP," + value)
+            else:
+                println("Invalid temperature: " + value)
         else:
-            println("Invalid temperature: " + value)
-    else:
-        println("Unrecognised message")
+            println("Unrecognised message")
+    except Exception, e:
+        println("Error: " + e)
 
 client = mqtt.Client()
 client.on_connect = on_connect
